a3d92d4000bb43c78ed439820cf2db13cca04e49
Vinod Nangare <vinodnangare8@gmail.com>
2026-09-25 15:29:47 +0530

    refact:App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import LoginPage from './pages/LoginPage';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import NotFoundPage from './pages/NotFoundPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import ProtectedRoute from './Component/ProtectedRoute';
-import ProductsPage from './pages/ProductsPage';
+import LoginPage from './Pages/LoginPage';
+import ProductsPage from './Pages/ProductsPage';
+import ProductDetailPage from './Pages/ProductDetailPage';
+import NotFoundPage from './Pages/NotFoundPage';
+
 const App = () => {
   return (
     <>
-     <Toaster position="top-right" />
-<BrowserRouter>
-    <Routes>
-
-         <Route
+      <Toaster position="top-right" />
+      <Routes>
+        <Route path="/" element={<Navigate to="/products" replace />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route
           path="/products"
           element={
             <ProtectedRoute>
@@ -20,12 +21,18 @@ const App = () => {
             </ProtectedRoute>
           }
         />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
- </BrowserRouter>
+        <Route
+          path="/products/:id"
+          element={
+            <ProtectedRoute>
+              <ProductDetailPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App  
+export default App;
